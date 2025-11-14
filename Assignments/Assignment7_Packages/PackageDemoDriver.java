206365c576ef9f77d542df6a4ce6b8553b26a458
Sia Gupta <guptasia.27@gmail.com>
2025-11-14 13:21:58 +0530

    Add files via upload

---
+++ Assignments/Assignment7_Packages/PackageDemoDriver.java
@@ -0,0 +1,8 @@
+package exercises.java;
+
+public class PackageDemoDriver{
+public static void main(String args[]) {
+PackageDemo demo= new PackageDemo();
+demo.display();
+System.out.println("SIA GUPTA 24CSU201");
+}}
---
+++ Assignments/Assignment7_Packages/PackageDemoMain.java
@@ -0,0 +1,8 @@
+import exercises.java.PackageDemo;
+
+public class PackageDemoMain {
+public static void main(String args[]) {
+PackageDemo d=new PackageDemo();
+d.display();
+System.out.println("SIA GUPTA 24CSU201");
+}}
